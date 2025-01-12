1cbe5a1731d021cc418e4c0b86f6ebf765f71f1b
Lucas <lucas.scotto.france@gmail.com>
2025-01-12 13:17:52 +0100

    projet

---
+++ core/src/main/java/com/project/game/GameMain.java
@@ -0,0 +1,41 @@
+package com.project.game;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/** This class serves only as the application scanning root. Any classes in its package (or any of the sub-packages)
+ * with proper Autumn MVC annotations will be found, scanned and initiated. */
+public class GameMain implements ApplicationListener {
+    /** Default application size. */
+    public static final int WIDTH = 450, HEIGHT = 600;
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
